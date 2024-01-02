a412d343ee3cbc649fa9ae1531924c77bd3dd2c4
Phate334 <phate334@gmail.com>
2024-01-02 17:38:31 +0800

    Remove OpenAI code

--- openai-test.py
+++
@@ -1,7 +0,0 @@
-from openai import OpenAI
-
-api_base = "https://openai.phate.dev/v1"
-api_key = "sk-template"
-openai = OpenAI(base_url=api_base, api_key=api_key)
-res = openai.completions.create(model="starling", prompt="This is a test", max_tokens=5)
-print(res)
